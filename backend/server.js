7bda79c4a8e841568ecd9b61c27b512d069896d9
Neel <lioneelsanghvi@gmail.com>
2021-05-09 19:10:31 +0530

    For Heroku

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,7 @@ app.use(express.json());
 app.use("/api/products", productsRoute);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+}
